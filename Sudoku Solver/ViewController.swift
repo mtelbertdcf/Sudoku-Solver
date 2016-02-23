98f92d7a2a013ab19b444271f742a55607894903
David Forney <david@scribblepost.com>
2016-02-23 10:58:21 -0800

    Initial Commit

---
+++ Sudoku Solver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sudoku Solver
+//
+//  Created by David on 2/23/16.
+//  Copyright © 2016 Mountainous Code LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
